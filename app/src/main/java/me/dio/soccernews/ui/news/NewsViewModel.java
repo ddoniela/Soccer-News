fc322b31c194fd4d60ef50731e0c24cc5fab8e44
Daniela Lima <sousa.danielalima@gmail.com>
2022-08-01 21:27:45 -0300

    first commit

---
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -0,0 +1,33 @@
+package me.dio.soccernews.ui.news;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dio.soccernews.domain.News;
+
+public class NewsViewModel extends ViewModel {
+
+    private final MutableLiveData<List<News>> news;
+
+    public NewsViewModel() {
+       this.news = new MutableLiveData<>();
+
+        //TODO remover mock de noticias
+        List<News>  news = new ArrayList<>();
+        news.add(new News("Botafogo tem desfalque importante", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News("Palmeiras joga sábado", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+        news.add(new News("Copa do Mundo feminina está terminando", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."));
+
+        this.news.setValue(news);
+
+    }
+
+    public LiveData<List<News>> getNews() {
+        return this.news;
+    }
+
+}
